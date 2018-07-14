411563b996f4a122d1c3f547bb0e6065ce9c4f87
Brian Pick <brianr.pick@gmail.com>
2018-07-14 14:22:29 -0700

    initial push

---
+++ v1/app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+
+
+
+app.get("/", function(req, res){
+  res.render("landing")
+})
+
+
+app.listen(3000, function() {
+  console.log("Serving app on port 3000");
+})
