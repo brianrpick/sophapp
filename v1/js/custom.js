eb1e0d990058159d03c2562a030b46880e49c662
Brian Pick <brianr.pick@gmail.com>
2018-07-15 13:42:07 -0700

    placeholder pictures, functioning navbar, working on name css

--- v1/js/custom.js
+++ v1/js/custom.js
@@ -2,13 +2,13 @@
 (function($) {
     'use strict';
 
-    // Main Navigation
-    $( '.hamburger-menu' ).on( 'click', function() {
-        $(this).toggleClass('close');
-        $('.site-branding').toggleClass('hide');
-        $('.site-navigation').toggleClass('show');
-        $('.site-header').toggleClass('no-shadow');
-    });
+  // Main Navigation
+  $( '.hamburger-menu' ).on( 'click', function() {
+      $(this).toggleClass('close');
+      $('.site-branding').toggleClass('hide');
+      $('.site-navigation').toggleClass('show');
+      $('.site-header').toggleClass('no-shadow');
+  });
 
     // Scroll to Next Section
     $( '.scroll-down' ).click(function() {
