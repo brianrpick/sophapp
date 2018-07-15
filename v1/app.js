d6c679f0c1b226526954aa974b08c43ccb22a287
Brian Pick <brianr.pick@gmail.com>
2018-07-15 11:05:57 -0700

    Adjusted views layout

--- v1/app.js
+++ v1/app.js
@@ -4,8 +4,8 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public/'));
 
-app.get("/work", function(req, res){
-  res.render("work")
+app.get("/about", function(req, res){
+  res.render("about")
 })
 
 app.get("/contact", function(req, res){
