44eda48ea9b3a3fe706e64f622a6033d1fc5fd33
Brian Pick <brianr.pick@gmail.com>
2018-07-14 18:58:11 -0700

    Nav bar up

--- v1/app.js
+++ v1/app.js
@@ -2,14 +2,12 @@ var express = require("express");
 var app = express();
 
 app.set("view engine", "ejs");
-
-
-
+app.use(express.static(__dirname + '/public/'));
 
 app.get("/", function(req, res){
   res.render("landing")
 })
-
+app.use(express.static("."));
 
 app.listen(3000, function() {
   console.log("Serving app on port 3000");
